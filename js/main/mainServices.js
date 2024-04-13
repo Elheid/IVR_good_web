76b83da4c800f6aad9a795a4f798da21b6fd0150
Elheid <Elfieleintsvard@mail.ru>
2024-04-13 22:20:20 +0500

    refactoring

--- js/main/mainServices.js
+++ js/main/mainServices.js
@@ -8,17 +8,6 @@ var flag = loadSavedData();
 var catalogs = document.querySelector(".catalogs").classList.add(flag);
 var services = document.querySelector(".services").classList.add(flag);
 
-const listOfCards = document.querySelectorAll(".list-of-cards");
-
-const twoInRow = document.querySelector(".two-in-row");
-const oneInRow = document.querySelector(".one-in-row");
-
-oneInRow.addEventListener("click", ()=>{
-    listOfCards.forEach((card)=> card.classList.add("list"));
-})
-twoInRow.addEventListener("click", ()=>{
-    listOfCards.forEach((card)=> card.classList.remove("list"));
-})
 
 initializeResults(4);
 addCatalogButton();
