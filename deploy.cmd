3f23b028722bf710b64c9d5d1b1cb61da818ffd1
Elheid <Elfieleintsvard@mail.ru>
2024-08-06 16:59:50 +0500

    addScript

---
+++ deploy.cmd
@@ -0,0 +1,13 @@
+@echo off
+REM Переменные для портов
+set PORT_MAIN=3000
+set PORT_ADMIN=3001
+
+REM Переменные для веток
+set MAIN="main"
+set ADMIN="adminPanelv2"
+
+
+start cmd /k "cd /d %cd% && git checkout %MAIN% && npx http-server -p %PORT_MAIN%"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d %cd% && git checkout %ADMIN% && npx http-server -p %PORT_ADMIN%"
