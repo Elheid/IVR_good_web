30d5cfbadb59fb1d034209c3d2f267276bfb7d11
Elheid <Elfieleintsvard@mail.ru>
2024-08-06 17:16:41 +0500

    refactor

--- deploy.cmd
+++ deploy.cmd
@@ -7,7 +7,6 @@ REM Переменные для веток
 set MAIN="main"
 set ADMIN="adminPanelv2"
 
-
-start cmd /k "cd /d %cd% && git checkout %MAIN% && timeout /t 1 /nobreak >nul && npx http-server -p %PORT_MAIN%"
-timeout /t 1 /nobreak >nul
 start cmd /k "cd /d %cd% && git checkout %ADMIN% && timeout /t 1 /nobreak >nul && npx http-server -p %PORT_ADMIN%"
+timeout /t 1 /nobreak >nul
+start cmd /k "cd /d %cd% && git checkout %MAIN% && timeout /t 1 /nobreak >nul && npx http-server -p %PORT_MAIN%"
