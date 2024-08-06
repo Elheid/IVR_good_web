34ad49760ae43aa352a9bab38927d3c0939bb33c
Elheid <Elfieleintsvard@mail.ru>
2024-08-06 17:07:58 +0500

    test

--- deploy.cmd
+++ deploy.cmd
@@ -9,5 +9,5 @@ set ADMIN="adminPanelv2"
 
 
 start cmd /k "cd /d %cd% && git checkout %MAIN% && npx http-server -p %PORT_MAIN%"
-timeout /t 1 /nobreak >nul
+timeout /t 5 /nobreak >nul
 start cmd /k "cd /d %cd% && git checkout %ADMIN% && npx http-server -p %PORT_ADMIN%"
