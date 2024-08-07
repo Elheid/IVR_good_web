959fd241d3be0dd97fda6159bb35fb2e2dd9aa77
Elheid <Elfieleintsvard@mail.ru>
2024-08-07 16:21:23 +0500

    refactor

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,11 @@
 @echo off
 
-REM Запуск первого сервера с adminPanelOn: true
-copy /Y configAdmin.js config.js
-start cmd /k "npx http-server -p 8080"
-
-timeout /t 5 /nobreak >nul
-
 REM Запуск второго сервера с adminPanelOn: false
 copy /Y configMain.js config.js
 start cmd /k "npx http-server -p 8081"
+
+timeout /t 5 /nobreak >nul
+
+REM Запуск первого сервера с adminPanelOn: true
+copy /Y configAdmin.js config.js
+start cmd /k "npx http-server -p 8080"
