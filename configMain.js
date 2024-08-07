23f5b5cd0e2556f687c4e345a3dec677e6fe0770
Elheid <Elfieleintsvard@mail.ru>
2024-08-07 17:03:00 +0500

    rechangeScript

--- configMain.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-    apiBackEndUrl: 'https://pincode-dev.ru/ivr-good/',
-    apiGesturalUrl: 'wss://pincode-dev.ru',
-    defaultGoHomeTimer:600000,
-    adminPanelOn:false,
-};
