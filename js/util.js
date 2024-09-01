839be99a30aff770fb88cc2bf567056ea826753e
Elheid <Elfieleintsvard@mail.ru>
2024-09-01 15:23:43 +0500

    refactor

--- js/util.js
+++ js/util.js
@@ -445,6 +445,7 @@ const getCurState = ()=>{
 //
 const isAdmin = ()=>{
     if (!config.adminPanelOn){
+        localStorage.removeItem("token");
         return false;
     }
     return window.localStorage.getItem("isAdmin") === "true"
