02498340b70b95fce52a78fcf1234b6aa9414e52
Elheid <Elfieleintsvard@mail.ru>
2024-09-21 17:37:49 +0500

    refactorResBlocks

--- js/form.js
+++ js/form.js
@@ -385,7 +385,8 @@ const updateFileInputAttributes = (listItem)=> {
     const uniqueId = `fileInputIcon${fileInputCounter}`;
     
     const fileInput = listItem.querySelector('input[type="file"]');
-    const fileLabel = listItem.querySelector('label[for="fileInputIcon1"]');
+    let fileLabel = listItem.querySelector('label.file-lable');
+    if (!fileLabel) fileLabel = listItem.querySelector('label[for="fileInputIcon2"]');
 
     if (fileInput && fileLabel) {
         fileInput.id = uniqueId;
